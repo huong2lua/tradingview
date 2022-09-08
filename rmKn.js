5e41f536e46411a15be2371475397bd4ad103a20
Hideki <61128858+huong2lua@users.noreply.github.com>
2022-09-08 20:11:29 +0700

    Update rmKn.js

--- rmKn.js
+++ rmKn.js
@@ -1,38 +1,19 @@
 // ==UserScript==
-// @name         Auto Connect Tradingview
+// @name         Tradingview auto clear auto connect
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  close the confirmnation pop when new location login
 // @author       You
 // @match        https://vn.tradingview.com/chart/VnvNenY8/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tradingview.com
 // @grant        none
 // ==/UserScript==
 
-function onElementInserted(containerSelector, elementSelector, callback) {
-    var onMutationsObserved = function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.addedNodes.length) {
-                var elements = $(mutation.addedNodes).find(elementSelector);
-                for (var i = 0, len = elements.length; i < len; i++) {
-                    callback(elements[i]);
-                }
-            }
-        });
-    };
-    var target = $(containerSelector)[0];
-    var config = { childList: true, subtree: true };
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-    var observer = new MutationObserver(onMutationsObserved);
-    observer.observe(target, config);
-
-}
-
-function setupAutoClickCloseButtonInside(selector) {
-  onElementInserted('body', selector, function(element) {
-    console.log("close " + selector);
-    $(selector + ' > button').click()
-  });
-}
-
-setupAutoClickCloseButtonInside('[data-dialog-name="gopro"]');
+const hideAds = setInterval(() => {
+  const adDialog = document.querySelector("div[data-dialog-name='gopro']");
+  if (adDialog)
+  {
+      adDialog.querySelector("button").click();
+      console.log('popup closed');
+  }
+}, 500);
