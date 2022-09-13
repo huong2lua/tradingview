fb3d90bdfee5bf7b115fb9dc9c40471731de0336
Hideki <61128858+huong2lua@users.noreply.github.com>
2022-09-13 10:45:10 +0700

    Update rmKn.js

--- rmKn.js
+++ rmKn.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Tradingview auto clear auto connect
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  close the confirmnation pop when new location login
-// @author       You
-// @match        https://vn.tradingview.com/chart/VnvNenY8/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tradingview.com
-// @grant        none
-// ==/UserScript==
-
 const hideAds = setInterval(() => {
   const adDialog = document.querySelector("div[data-dialog-name='gopro']");
   if (adDialog)
