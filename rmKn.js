7506c067e838d0c5e8cdac405de19860764b2c98
huongnv <hulu1221@gmail.com>
2025-11-05 09:13:56 +0700

    update script

--- rmKn.js
+++
@@ -1,8 +0,0 @@
-const hideAds = setInterval(() => {
-  const adDialog = document.querySelector("div[data-dialog-name='gopro']");
-  if (adDialog)
-  {
-      adDialog.querySelector("button").click();
-      console.log('popup closed');
-  }
-}, 10);
